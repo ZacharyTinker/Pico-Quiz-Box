c9925e174316f33381d762714dc7188b56acd692
Ryan Longacre <ryanlongacre@outlook.com>
2022-07-03 21:45:58 -0400

    debug: Add debugging print statements

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ quizzers = [
     Quizzer("Team 3 Seat 4", 12, 27)
 ]
 
+print("Starting up...")
+
+print(f"Current state is {current_state}")
 
 while True:
     if current_state == STATE_RESET:
@@ -35,19 +38,27 @@ while True:
                 quizzer.light(True)
                 qm_led.on()
                 current_state = STATE_LIVE
+                print(f"{quizzer.name} pressed button, state is now {current_state}")
                 break
         if reset.is_toggled():
             current_state = STATE_TEST
+            print(f"Reset button is toggled, state is now {current_state}")
             qm_led.on()
     elif current_state == STATE_TEST:
         for quizzer in quizzers:
-            quizzer.light(quizzer.pressed())
+            if quizzer.buzzer():
+                print(f"{quizzer.name} pressed button")
+                quizzer.light(True)
+            else:
+                quizzer.light(False)
         if reset.is_toggled():
             for quizzer in quizzers:
                 quizzer.light(False)
             qm_led.off()
             current_state = STATE_RESET
+            print(f"Reset button is toggled, state is now {current_state}")
     elif current_state == STATE_LIVE:
         if reset.is_toggled():
             qm_led.off()
             current_state = STATE_RESET
+            print(f"Reset button is toggled, state is now {current_state}")
