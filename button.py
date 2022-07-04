26463f8167401e3269083ec8f1ee0c88504ea2dc
Ryan Longacre <ryanlongacre@outlook.com>
2022-07-03 22:37:51 -0400

    feat: Implement toggle function

--- button.py
+++ button.py
@@ -1,13 +1,20 @@
 from machine import Pin
+from time import time
 
 class Button:
     def __init__(self, pin_number):
         self.pin_number = pin_number
         self.pin = Pin(self.pin_number, Pin.IN, Pin.PULL_DOWN)
+        self.last_pressed = 0 
 
     def is_pressed(self):
-        return bool(self.pin.value())
+        if bool(self.pin.value()):
+            self.last_pressed = round(time() * 1000)
+            return True
+        else:
+            return False
 
     def is_toggled(self):
-        # TODO: Return true if the button was recently pressed and released
-        return False
+        last_pressed = self.last_pressed
+        pressed = self.is_pressed()
+        return not pressed and (self.last_pressed - last_pressed < 50)
